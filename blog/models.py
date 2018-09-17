347e67f8142819ee9b9eecf6b9df935a7c5f036e
unknown <ksinghasane14@gmail.com>
2018-09-17 14:51:06 +0530

    blog

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=60)
-    date = models.DateField(auto_now_add=True)
-    summary = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    summary = models.TextField()
     image = models.ImageField(upload_to='images/')
 
