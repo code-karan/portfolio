083b6594ba853a7d979917047b02932710f4fbfd
Karan Singhasane <ksinghasane14@gmail.com>
2019-10-20 20:49:05 +0530

    Completed

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class Blog(models.Model):
     summary = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+    
+    def new_summary(self):
+        return self.summary[:200] + ' ...Click to read more'
+
+    def new_date(self):
+        return self.date.strftime('%b %e, %Y')
+
+
